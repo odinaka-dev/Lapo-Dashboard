526e1082dfb5d1bb1387818103c0a9335adb2a32
uchechukwu <uchechukwupeter22@gmail.com>
2025-02-22 10:55:41 +0100

    adding errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Card from "./pages/Card.jsx";
 import Layout from "./components/Layout.jsx";
+import Error from "./pages/Error.jsx";
 import ComplaintsDetails from "./components/ComplaintsDetails.jsx";
 
 // React Router
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorComponent = () => {
+  return (
+    <section>
+      <h1>404 error</h1>
+    </section>
+  );
+};
+
+export default ErrorComponent;
